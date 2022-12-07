4f77ab87aa88af43b13b221dda64dc2dfd399f86
DannyDoneva96 <danielasinmas1@gmail.com>
2022-12-07 16:10:01 +0200

    update contact us
    aded image

--- client/pizza-site/src/app/pages/about/about.component.ts
+++ client/pizza-site/src/app/pages/about/about.component.ts
@@ -9,16 +9,17 @@ import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 export class AboutComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {}
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
   center: google.maps.LatLngLiteral = {
       lat: 24,
       lng: 12
   };
-  zoom = 4;
-  markerOptions: google.maps.MarkerOptions = {
-      draggable: false
-  };
   markerPositions: google.maps.LatLngLiteral[] = [];
+  zoom = 4;
   addMarker(event: google.maps.MapMouseEvent) {
       if (event.latLng != null) this.markerPositions.push(event.latLng.toJSON());
+  }
+  openInfoWindow(marker: MapMarker) {
+      if (this.infoWindow != undefined) this.infoWindow.open(marker);
   }
 }
