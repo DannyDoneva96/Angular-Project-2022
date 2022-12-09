36c2b9208b5aa4927c4123b766bbeeec21530307
DannyDoneva96 <danielasinmas1@gmail.com>
2022-12-09 15:57:36 +0200

    update product details page

--- client/pizza-site/src/app/pages/product-details/product-details.component.ts
+++ client/pizza-site/src/app/pages/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component ,OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {OrderDetailsService} from 'src/app/services/order-details.service'
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -7,12 +9,20 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor(private param:ActivatedRoute){
+  constructor(private param:ActivatedRoute,private service:OrderDetailsService){
 
   }
   getId:any
-
+  productData:any
    ngOnInit(): void {
     this.getId = this.param.snapshot.paramMap.get('id');
-   }
+    // console.log(this.getId);
+    if (this.getId){
+      this.service.foodDetails.filter((product:any) =>{
+        if(product.id==this.getId){
+          this.productData= product
+        }
+      })
+    }
+  }
 }
