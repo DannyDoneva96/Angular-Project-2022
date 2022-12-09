0b73a802d36b7c5030d672db77a9ccc7a0db1e50
DannyDoneva96 <danielasinmas1@gmail.com>
2022-12-09 09:53:26 +0200

    updates
    add  styles to login page
    add food details json

--- client/pizza-site/src/app/services/order-details.service.ts
+++ client/pizza-site/src/app/services/order-details.service.ts
@@ -6,4 +6,44 @@ import { Injectable } from '@angular/core';
 export class OrderDetailsService {
 
   constructor() { }
+
+  //Food details 
+  foodDetails = [
+    { id:1,
+      category:'pizza',
+       name:'',
+      size:'',
+      price:'',
+      quantity:'',
+      
+    }, { id:2,
+      category:'pizza',
+       name:'',
+      size:'',
+      price:'',
+      quantity:'',
+      
+    }, { id:3,
+      category:'pizza',
+       name:'',
+      size:'',
+      price:'',
+      quantity:'',
+      
+    }, { id:4,
+      category:'pizza',
+       name:'',
+      size:'',
+      price:'',
+      quantity:'',
+      
+    }, { id:5,
+      category:'pizza',
+       name:'',
+      size:'',
+      price:'',
+      quantity:'',
+      
+    }
+  ]
 }
