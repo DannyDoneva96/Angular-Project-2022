404c3e689a290f8cb9a2a8d6ea7944354d9be822
DannyDoneva96 <danielasinmas1@gmail.com>
2022-12-10 15:27:18 +0200

    updates
    created all the request :  add , get all, edit and delete messages

--- client/pizza-site/src/app/data.service.ts
+++ client/pizza-site/src/app/data.service.ts
@@ -15,4 +15,23 @@ export class DataService {
     message.id = this.afs.createId()
     return this.afs.collection('/messages').add(message)
   }
+
+ // get all messages
+
+ getAllMessages(){
+  return this.afs.collection('/messages').snapshotChanges()
+ }
+
+ //delete message
+
+ deleteMessage(message:Message){
+  return this.afs.doc('/messages/' + message.id).delete()
+ }
+
+ //update message
+ updateMessage(message:Message){
+  this.deleteMessage(message);
+  this.addMessage(message);
+ }
+
 }
