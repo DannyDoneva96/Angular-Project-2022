0fc5d389ddd70ad8eabe5018c3cd6026dc2f6830
DannyDoneva96 <danielasinmas1@gmail.com>
2022-12-16 15:30:36 +0200

    make order services

--- client/pizza-site/src/app/pages/orders/orders.component.ts
+++ client/pizza-site/src/app/pages/orders/orders.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../pages/auth/auth.service';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+       isAdmine: boolean = false;
+       isOwner: boolean = false;
+
+  constructor(public auth: AuthService) { }
+
+
+  ngOnInit() { 
+    if(localStorage.getItem('user')!==null){
+      const user = JSON.parse(localStorage.getItem('user')!);  
+      if(user.email == 'admin@abv.bg'){
+        this.isAdmine = true;
+      }
+    }
+    
+     
+  }
+
+
+
 
 }
