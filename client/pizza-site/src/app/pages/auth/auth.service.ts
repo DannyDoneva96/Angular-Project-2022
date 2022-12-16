cbecdac016a0eab43da689549948a1e4495ec2bc
DannyDoneva96 <danielasinmas1@gmail.com>
2022-12-16 15:09:26 +0200

    create user in local storage

--- client/pizza-site/src/app/pages/auth/auth.service.ts
+++ client/pizza-site/src/app/pages/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import {User } from './user'
+import * as auth from 'firebase/auth';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,12 +11,28 @@ export class AuthService {
 
   isLoggedIn: boolean = false;
   errorMessage: null | string = null;
+  userData: any; // Save logged in user data
 
-  constructor(private fireauth: AngularFireAuth, private router: Router) { }
+
+  constructor(private fireauth: AngularFireAuth, private router: Router){
+    /* Saving user data in localstorage when 
+    logged in and setting up null when logged out */
+    this.fireauth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
+}
 
   //login method
   login(email: string, password: string) {
-    this.fireauth.signInWithEmailAndPassword(email, password).then(() => {
+    this.fireauth.signInWithEmailAndPassword(email, password).then((result) => {
+      this.SetUserData(result.user);
       localStorage.setItem('token', 'true');
       this.isLoggedIn = true;
       this.router.navigate(['/']);
@@ -24,7 +43,19 @@ export class AuthService {
     })
   };
 
-  
+  /* Setting up user data when sign in with username/password, 
+  sign up with username/password and sign in with social auth  
+  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
+  SetUserData(user: any) {
+   
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+     
+    };
+    
+   
+  }
   
   register(email: string,password: string){
     this.fireauth.createUserWithEmailAndPassword(email,password).then(()=>{
@@ -36,6 +67,7 @@ export class AuthService {
 
   logout() {
       this.fireauth.signOut().then(() => {
+        localStorage.removeItem('user')
         localStorage.removeItem('token')
         this.isLoggedIn = false
         this.router.navigate(['/login']);
